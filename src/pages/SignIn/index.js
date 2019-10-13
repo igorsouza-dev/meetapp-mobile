1652401b1efa25f29f725e108c1263606d3fd84b
Igor Souza <igorsouza.dev@gmail.com>
2019-10-13 08:34:51 -0300

    Added sign in page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,62 @@
-import React from 'react';
-
-import { Container } from './styles';
+import React, { useState, useRef } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 import Background from '~/components/Background';
+import Logo from '~/components/Logo';
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const passwordRef = useRef();
+  function handleSignIn() {}
   return (
     <Background>
-      <Container />
+      <Container>
+        <Logo />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Insert your e-mail"
+            value={email}
+            onChangeText={setEmail}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Insert your password"
+            value={password}
+            ref={passwordRef}
+            onChangeText={setPassword}
+            returnKeyType="send"
+            onSubmitEditing={handleSignIn}
+          />
+          <SubmitButton onPress={handleSignIn}>Sign In</SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Create a free account</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
+
+SignIn.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func.isRequired,
+  }).isRequired,
+};
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,3 +1,37 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
+import Button from '~/components/Button';
+import Input from '~/components/Input';
 
-export const Container = styled.View``;
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavior: 'padding',
+})`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 20px;
+`;
+
+export const Form = styled.View`
+  align-self: stretch;
+  margin-top: 50px;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const SignLink = styled.TouchableOpacity`
+  margin-top: 20px;
+`;
+
+export const SignLinkText = styled.Text`
+  color: #fff;
+  font-size: 16px;
+  font-weight: bold;
+`;
