3a3aed8475944f5e8a49605d6861d12711e8300e
Igor Souza <igorsouza.dev@gmail.com>
2019-10-22 12:33:10 -0300

    Added codepush. Added push notifications

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StatusBar } from 'react-native';
+import CodePush from 'react-native-code-push';
+import OneSignal from 'react-native-onesignal';
 
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
@@ -8,13 +10,39 @@ import '~/config/ReactotronConfig';
 import { store, persistor } from './store';
 import App from './App';
 
-export default function Index() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <StatusBar barStyle="light-content" backgroundColor="#22202C" />
-        <App />
-      </PersistGate>
-    </Provider>
-  );
+class Index extends Component {
+  constructor(props) {
+    super(props);
+    OneSignal.init('c135a8d2-f0e0-4071-a596-3f56042925c3');
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => { };
+
+  onOpened = notification => { };
+
+  onIds = id => { };
+
+  render() {
+    return (
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <StatusBar barStyle="light-content" backgroundColor="#22202C" />
+          <App />
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
+
+export default CodePush({
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+})(Index);
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://10.5.1.200:3333',
+  baseURL: 'http://134.209.175.247',
 });
 
 export default api;
